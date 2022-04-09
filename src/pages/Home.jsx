0baf7ea95f5011daef9b76cb42ff8408668d3c7a
Hammad Parveez <hammadpervez6@gmail.com>
2022-04-09 22:14:16 +0500

    designing netflix clone

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+export default function Home() {
+    return (
+        <>
+            <div className="wrapper">
+                <Header />
+                {/* Banner Section */}
+                <section className="banner">
+                    <h1>Unlimited movies, TV<br />shows, and more.</h1>
+                    <h4>Watch anywhere. Cancel anytime.</h4>
+                    <p>Ready to watch? Enter your email to create or restart your membership.</p>
+                </section>
+            </div>
+        </>
+    );
+}
+
+const Header = () => {
+    return <header className="d-flex justify-content-between align-items-start" >
+        <img src="/images/netflix.svg" alt="Netflix" />
+        <a href="#"> Sign In </a>
+    </header>
+}
