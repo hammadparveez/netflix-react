5483c67b5ecbcb636bdf494ef30f2b3444b7b4f9
Hammad Parveez <hammadpervez6@gmail.com>
2022-04-09 22:43:21 +0500

    designed banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ export default function Home() {
                 <Header />
                 {/* Banner Section */}
                 <section className="banner">
+                    <div className="overlay-backgroud">
+                        <img srcSet="/images/netflix_bg.jpeg" alt="" />
+                    </div>
                     <h1>Unlimited movies, TV<br />shows, and more.</h1>
                     <h4>Watch anywhere. Cancel anytime.</h4>
                     <p>Ready to watch? Enter your email to create or restart your membership.</p>
